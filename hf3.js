cd362d4875168e297ffd878c897c4f0c0b0f86d7
Wexp08 <muhidinovmurtazo08@gmail.com>
2024-10-06 11:02:55 +0500

    Add files via upload

---
+++ hf3.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Плавная прокрутка к разделам
+    const links = document.querySelectorAll('nav a');
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href');
+            document.querySelector(targetId).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
